e6fc11b2cb31315e643d4d5ed936106d2bbe4b95
ablaszkiewicz <aleksander@posthog.com>
2025-09-10 18:21:19 +0200

    feat: make templates reusable in CDP and other tabs

--- plugin-server/src/cdp/templates/_destinations/github/github.template.ts
+++ plugin-server/src/cdp/templates/_destinations/github/github.template.ts
@@ -10,45 +10,36 @@ export const template: HogFunctionTemplate = {
     icon_url: '/static/services/github.png',
     category: ['Error tracking'],
     code_language: 'hog',
-    code: `fun create_issue() {
-    let owner := inputs.github_installation.account.name
-    let repo := inputs.repository
-    let posthog_issue_id := event.distinct_id
+    code: `let owner := inputs.github_installation.account.name
+let repo := inputs.repository
 
-    if (not owner) {
-        throw Error('Owner is required')
-    }
+if (not owner) {
+    throw Error('Owner is required')
+}
 
-    if (not repo) {
-        throw Error('Repository is required')
-    }
+if (not repo) {
+    throw Error('Repository is required')
+}
 
-    if (not posthog_issue_id) {
-        throw Error('PostHog issue ID is required')
-    }
-
-    let posthog_issue_url := f'{project.url}/error_tracking/{posthog_issue_id}'
-    let payload := {
-        'method': 'POST',
-        'headers': {
-            'Authorization': f'Bearer {inputs.github_installation.access_token}',
-            'Accept': 'application/vnd.github+json',
-            'Content-Type': 'application/json',
-            'User-Agent': 'PostHog Github App'
-        },
-        'body': {
-            'title': inputs.title,
-            'body': f'{inputs.description}\n\n[View in PostHog]({posthog_issue_url})'
-        }
-    }
-
-    let res := fetch(f'https://api.github.com/repos/{owner}/{repo}/issues', payload)
-    if (res.status < 200 or res.status >= 300) {
-        throw Error(f'Failed to create GitHub issue: {res.status}: {res.body}')
+let posthog_issue_url := f'{project.url}/error_tracking/{inputs.posthog_issue_id}'
+let payload := {
+    'method': 'POST',
+    'headers': {
+        'Authorization': f'Bearer {inputs.github_installation.access_token}',
+        'Accept': 'application/vnd.github+json',
+        'Content-Type': 'application/json',
+        'User-Agent': 'PostHog Github App'
+    },
+    'body': {
+        'title': inputs.title,
+        'body': f'{inputs.description}\n\n[View in PostHog]({posthog_issue_url})'
     }
 }
 
-create_issue();`,
+let res := fetch(f'https://api.github.com/repos/{owner}/{repo}/issues', payload)
+if (res.status < 200 or res.status >= 300) {
+    throw Error(f'Failed to create GitHub issue: {res.status}: {res.body}')
+}`,
     inputs_schema: [
         {
             key: 'github_installation',
@@ -75,6 +66,7 @@ create_issue();`,
             label: 'Title',
             secret: false,
             hidden: false,
+            required: true,
             default: '{event.properties.$exception_types[1]}',
         },
         {
@@ -83,7 +75,17 @@ create_issue();`,
             label: 'Description',
             secret: false,
             hidden: false,
+            required: true,
             default: '{event.properties.$exception_values[1]}',
+        },
+        {
+            key: 'posthog_issue_id',
+            type: 'string',
+            label: 'PostHog issue ID',
+            secret: false,
+            hidden: false,
+            required: true,
+            default: '{event.properties.$exception_issue_id}',
         },
     ],
 }
